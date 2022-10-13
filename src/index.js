1ff8aae5707adbb63ab5b2e4d99a8e6d814bd0b5
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-10-13 20:02:11 +0200

    Update index.js
    Fetching API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-//Boots up the app
-function init() {
+//Consts
+const apikey = "8d99168320785ca69d269538057bbbfa"
+const apiEndpoint = "https://api.themoviedb.org/3"
 
+const apiPaths = {
+    fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`
+}
+
+//Boots up the app 
+function init() {
+    fetchAndBuildAllSections();
+}
+
+function fetchAndBuildAllSections() {
+    fetch(apiPaths.fetchAllCategories)
+    .then(res => res.json())
+    .then(res => {
+        const categories = res.genres;
+        if (Array.isArray(categories) && categories.length) {
+            categories.forEach(category => {
+                fetchAndBuildAllSections(category);
+            });
+        }
+        //console.table(movies);
+    })
+    .catch(err=>console.error(err));
+}
+
+function fetchAndBuildMovieSection(category) {
+    console.log(category)
 }
 
 window.addEventListener('load', function() {
