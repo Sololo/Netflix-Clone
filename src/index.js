0b29e995246a5d7101d5aa730ba5402670829191
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-10-15 19:58:24 +0200

    Updated HTML CSS JavaScript
    Added 2 Images in Images folder

--- src/index.js
+++ src/index.js
@@ -28,9 +28,11 @@ function fetchTrendingMovies(){
 
 function buildBannerSection(movie){
     const bannerCont = document.getElementById('banner-section');
+
     bannerCont.style.backgroundImage = `url(${imgPath}${movie.backdrop_path})`;
 
     const div = document.createElement('div');
+
     div.innerHTML = `
             <h2 class="banner_title">${movie.title}</h2>
             <p class="banner_info">Trending in movies | Released - ${movie.release_date}</p>
@@ -38,9 +40,10 @@ function buildBannerSection(movie){
             <div class = "action-buttons-cont">
                 <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> &nbsp;&nbsp; Play</button>
                 <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg>&nbsp;&nbsp; More info</button>
-            </div>`
+            </div>
+        `;
             
-        div.className = "banner content container";
+        div.className = "banner-content container";
     
     bannerCont.append(div);
 }
@@ -55,7 +58,7 @@ function fetchAndBuildAllSections() {
                 fetchAndBuildMovieSection(
                     apiPaths.fetchMoviesList(category.id),
                     category.name
-                    );
+                );
             });
         }
         //console.table(movies);
@@ -71,14 +74,12 @@ function fetchAndBuildMovieSection(fetchUrl, categoryName) {
         //console.log(res.results);
         const movies = res.results;
         if(Array.isArray(movies) && movies.length) {
-           buildMoviesSection(movies.slice(0,6), categoryName);
+           buildMoviesSection(movies.slice(0,10), categoryName);
         }
         return movies;
     })
     .catch(err => console.error(err))
 }
-
-
 
 function buildMoviesSection(list, categoryName) {
     console.log(list, categoryName);
@@ -87,26 +88,23 @@ function buildMoviesSection(list, categoryName) {
 
     const moviesListHTML = list.map(item =>{
         return `
-        <div class = "movie-item" onmouseover="searchMovieTrailer('${item.title}', 'yt${item.id}')">
+        <div class = "movie-item" onmouseenter="searchMovieTrailer('${item.title}', 'yt${item.id}')">
             <img class= "movie-item-img " src="${imgPath}${item.backdrop_path}" alt="${item.title}" />
-            <iframe width="45" height="150" src="" id="yt${item.id}"></iframe>
+            <div class= "iframe-wrap" id="yt${item.id}"></div>
         </div>`;
     }).join('');
 
     const moviesSectionHTML = `
-    <div class = "movies-section">
-        <h2 class="movie-section-heading">${categoryName}<span class = "explore-nudge"> Explore All</span></h2>
+        <h2 class="movie-section-heading">${categoryName} <span class ="explore-nudge">Explore All</span></h2>
         <div class="movies-row">
             ${moviesListHTML}
-        <div>
-    </div>
+        </div>
     `
 
-    console.log(moviesSectionHTML);
 
     const div = document.createElement('div');
     div.className = "movies-section"
-    div.innerHTML =moviesSectionHTML;
+    div.innerHTML = moviesSectionHTML;
 
     // append html into movies container
     moviesCont.append(div);
@@ -119,10 +117,14 @@ function searchMovieTrailer(movieName, iframeId) {
     .then(res => res.json())
     .then(res =>{
         const bestResult = res.items[0];
-        const youtubeUrl = `https://www.youtube.com/watch?v=${bestResult.id.videoId}`;
-        console.log(youtubeUrl);
+
         const elements = document.getElementById(iframeId);
-        elements.src = `https://www.youtube.com/embed/${bestResult.id.videoId}?autoplay=1&controls=0`;
+        console.log(elements, iframeId);
+
+        const div = document.createElement('div');
+        div.innerHTML = `<iframe width="245px" height= "150px" src= "https://www.youtube.com/embed/${bestResult.id.videoId}?autoplay=1&controls=0"></iframe>`;
+
+        elements.append('div');
     })
     .catch(err => console.log(err));
 }
